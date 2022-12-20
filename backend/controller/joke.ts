bccd0fa2279d3a801ce4eb4c527f3297dc20da5e
gemamur <33229517+gemamur@users.noreply.github.com>
2022-12-21 00:16:37 +0100

    typescript update

--- backend/controller/joke.ts
+++ backend/controller/joke.ts
@@ -1,20 +1,20 @@
 import Joke from "../model/joke.model";
 
-export const countAllJokes = async(req,res) => {
+export const countAllJokes = async(req: any,res: { send: (arg0: { total: number; }) => void; }):Promise<void>=> {
     const jokes = await Joke.findAndCountAll();
     res.send({
         total: jokes.count
     });
 };
 
-export const getJoke = async(req,res)=>{
+export const getJoke = async(req: { params: { id: number; }; },res: { send: (arg0: Joke) => void; sendStatus: (arg0: number) => void; })=>{
     const id = req.params.id;
     await Joke.findOne({ where: {id: id}}).then((item)=> {
         item ? res.send(item) : res.sendStatus(404);
     });
 };
 
-export const saveJoke = async (req,res) => {
+export const saveJoke = async (req: { body: { type: string; setup: string; punchline: string; }; },res):Promise<void> => {
     const joke = {
 
         type: req.body.type,
